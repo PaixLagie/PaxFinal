b2556694b45ec36c7107690e61d574314656af07
PaixLagie <teva.marbaud@hotmail.com>
2023-08-20 18:00:31 +0200

    2eme itération Tev

--- app/src/main/java/com/PaixLagie/paxfinal/MainActivity.kt
+++ app/src/main/java/com/PaixLagie/paxfinal/MainActivity.kt
@@ -14,7 +14,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
+
+
+
 
 
 
@@ -47,13 +56,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             modifier = modifier
         )
         Image(
-            painter = painterResource(id = R.drawable.Ven), // Assurez-vous que Ven.jpg est dans le dossier res/drawable de votre projet
-            contentDescription = "Image Ven",
+            painter = painterResource(id = R.drawable.ven), // Assurez-vous que ven.jpg est dans le dossier res/drawable de votre projet
+            contentDescription = "Image ven",
             modifier = Modifier
 
 
         )
     }
+}
+@Composable
+fun AnimatedImage() {
+    var scale by remember { mutableStateOf(1f) }
+
+    val animatedScale by animateFloatAsState(
+        targetValue = if (scale == 1f) 1.2f else 1f,
+        animationSpec = tween(1000) // Réglage de la durée de l'animation en millisecondes
+    )
+
+    scale = animatedScale
+
+    Image(
+        painter = painterResource(id = R.drawable.ven), // Assurez-vous d'utiliser le bon nom de fichier
+        contentDescription = "Image Ven",
+        modifier = Modifier
+            .size(200.dp)
+            .graphicsLayer(scaleX = scale, scaleY = scale)
+    )
 }
 
 @Preview(showBackground = true)
